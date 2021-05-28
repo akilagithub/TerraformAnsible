8fb392e769a319ecd93b0660212785ec5fc149a7
user1 <user1@ldap.com>
2021-05-28 10:44:44 +0530

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+key_name = "terraform"                                                                                                                                                                        private_key_path = "terraform.pem"
+ssh_user = "ec2-user"
+private_key_path = "terraform.pem"
---
+++ variables.tf
@@ -0,0 +1,37 @@
+
+####################### Variables #############################
+
+variable "private_key_path"{
+  type = string
+  default = "terraform.pem"
+}  
+
+variable "ssh_user"{
+  type = string
+  default = "ec2-user" 
+}
+
+variable "key_name"{
+  type = string
+  description = "key name"
+  default = "terraform" 
+}
+
+variable "region" {                                                                                                                                                                             type    = string
+  default = "eu-west-1"
+  description = "Name of the region to create resource"
+}
+
+variable "network_address_space" {                               
+     default = "10.1.0.0/16"              
+}
+
+variable "subnet_address_space" {                               
+     default = "10.1.0.0/24"       
+}
+
+
+
+
+
+
